572d4e82cf231f688fe5e8ba88c96ef02286a50e
Gabriel Pinheiro <gabriel.pds2001@gmail.com>
2022-11-08 17:24:55 -0300

    init project

---
+++ src/routes/mainRoute.ts
@@ -0,0 +1,7 @@
+import {Router, Request, Response} from 'express';
+
+const router = Router();
+
+router.get('/ping', (req: Request, res: Response)=>{
+    res.json({pong: true});
+})
---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import fileUpload from 'express-fileupload';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import {resolve} from 'path';
+
+dotenv.config();
+const port = process.env.PORT;
+
+mongoose.connect(process.env.DATABASE_URI as string)
+    .then(()=>{ console.log('Deu certo')})
+    .catch((err)=>{console.log('ERROR DE CONEXAO\N', err.message)});
+
+
+const app = express();
+
+app.use(cors());
+app.use(fileUpload());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(resolve(__dirname, '..', 'public')));
+
+app.listen(port, ()=>{
+    console.log(`Rodando servidor em: http://localhost:${port}`);
+})
